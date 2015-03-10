74cd5a05fd9e254475284356abe05f4469f090e1
koekeishiya <Koe@pc160-142.eduroam.uib.no>
2015-03-10 18:04:00 +0100

    made a separate class for creating a CLI interface of this client

--- ncClient.java
+++ ncClient.java
@@ -76,33 +76,4 @@ public class ncClient {
       public Socket connection() throws Exception {
             return connection;
       }
-
-      // Create a new client and establish a connection to the given host
-      public static void main(String[] args) {
-            try {
-                  BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-                  System.out.print("Connect to host:");
-
-                  String host = br.readLine();
-                  int port = 1337;
-
-                  ncClient client = new ncClient(host,port);
-
-                  // Connected successfully, query user for messages
-                  boolean exit = false;
-                  do {
-                        String msg = br.readLine();
-                        // Aborted by user?
-                        if(msg.equals("/quit") || msg.equals("/exit")) {
-                              exit = true;
-                              break;
-                        }
-                        client.sendMessage(msg);
-                  } while(!exit);
-                  client.connection().close();
-            } catch (Exception ex) {
-                  System.out.println("An error has occurred! Aborting...");
-            }
-            System.exit(0);
-      }
 }
