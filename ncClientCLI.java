1ab92e84153c0820260d10aa054cfa92e0c40a79
koekeishiya <Koe@Asmunds-MacBook-Pro.local>
2015-03-11 00:47:14 +0100

    enabled start of debug mode using -DEBUG ,the CLI (or GUI for that matter) now have to handle messages using the ncClient.readMessage() function

--- ncClientCLI.java
+++ ncClientCLI.java
@@ -6,12 +6,26 @@ public class ncClientCLI {
       public static void main(String[] args) {
             try {
                   BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-                  System.out.print("Connect to host:");
+                  System.out.print("Connect to host: ");
 
                   String host = br.readLine();
                   int port = 1337;
 
-                  ncClient client = new ncClient(host,port);
+                  ncClient client = null;
+
+                  // Debug flag prints messages to syso
+                  if(args.length >0){
+                        // Should enable debug messages
+                  	if(args[0].toUpperCase().equals("-DEBUG")){
+                              client = new ncClient(host,port,true);
+                        }
+                  }else{
+                        // Do not enable debug messages
+                        client = new ncClient(host,port,false);
+                  }
+
+
+                  msgHandler(client);
 
                   // Connected successfully, query user for messages
                   boolean exit = false;
@@ -32,5 +46,24 @@ public class ncClientCLI {
                   System.out.println("An error has occurred! Aborting...");
             }
             System.exit(0);
+      }
+
+      // Listens to incoming messages from server, on a new thread
+      public static void msgHandler(ncClient client) {
+            (new Thread(){
+                  @Override
+                  public void run() {
+                  while(true) {
+                        try {
+                              // Gets message from server
+                              System.out.println(client.readMessage());
+
+                        } catch (Exception ex) {
+                              System.out.println("Connection Error! Aborting...");
+                              System.exit(0);
+                        }
+                  }
+                  }
+            }).start();
       }
 }
